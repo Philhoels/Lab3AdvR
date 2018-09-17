f170d3675c9ac2b8fe119ea22947be04a0e7e085
DucDuong92 <minhducduong92@gmail.com>
2018-09-17 16:16:55 +0200

    Update Wiki_graph doc

--- R/wiki_graph.R
+++ R/wiki_graph.R
@@ -1,9 +1,9 @@
-#' Prices of 50,000 round cut diamonds.
+#' Wiki_graph data
 #'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds.
+#' A sample dataset using for dijkstra function.
+#' 
 #'
-#' @format A data frame with 53940 rows and 10 variables:
+#' @format A data frame with 18 rows and 3 variables:
 #' \describe{
 #'   \item{v1}{price, in US dollars}
 #'   \item{v2}{weight of the diamond, in carats}
